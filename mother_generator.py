4933fc4ac7191225585bac15527a2826aab574ee
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-10 15:14:27 -0300

    Coreção gerador de nome de mãe

--- mother_generator.py
+++ mother_generator.py
@@ -166,30 +166,34 @@ unique_mother_surnames = list(set(mother_surnames))
 
 
 def generate_mother_by_name(full_name):
-    num_mother_names = random.choices([1, 2], weights=[75, 25])[0]
-    num_mother_surnames = random.choices([1, 2], weights=[35, 65])[0]
-    shared_num_mother_surnames = random.choices([0, 1, 2], weights=[30, 65, 5])[0]
-    first_mother_name = random.sample(unique_mother_names, num_mother_names)
-    # Compartilhando pelo menos um sobrenome com o perfil principal
-    shared_surname = random.choice(full_name.split()[1:])
-    shared_last_names = random.sample(shared_surname, shared_num_mother_surnames)
-    mother_last_names = random.sample(unique_mother_surnames, num_mother_surnames)
-    full_mother_name = ' '.join(first_mother_name + mother_last_names + shared_last_names)
-
     # num_mother_names = random.choices([1, 2], weights=[75, 25])[0]
-    # num_mother_surnames = random.choices([1, 2, 3, 4], weights=[25, 40, 25, 10])[0]
-    # shared_num_mother_surnames = min(num_mother_surnames, len(full_name.split()) - 1)
-    
-    # # Primeiro nome da mãe
+    # num_mother_surnames = random.choices([1, 2], weights=[35, 65])[0]
+    # shared_num_mother_surnames = random.choices([0, 1, 2], weights=[30, 65, 5])[0]
     # first_mother_name = random.sample(unique_mother_names, num_mother_names)
+    # # Compartilhando pelo menos um sobrenome com o perfil principal
+    # shared_surname = random.choice(full_name.split()[1:])
+    # shared_last_names = random.sample(shared_surname, shared_num_mother_surnames)
+    # mother_last_names = random.sample(unique_mother_surnames, num_mother_surnames)
+    # full_mother_name = ' '.join(first_mother_name + mother_last_names + shared_last_names)
+
+    num_mother_names = random.choices([1, 2], weights=[75, 25])[0]
+    num_mother_surnames = random.choices([1, 2, 3, 4], weights=[25, 40, 25, 10])[0]
+    shared_num_mother_surnames = min(num_mother_surnames, len(full_name.split()) - 1)
     
-    # # Sobrenomes compartilhados
-    # shared_surnames = random.sample(list(set(full_name.split()[1:])), shared_num_mother_surnames)
+    # Primeiro nome da mãe
+    first_mother_name = random.sample(unique_mother_names, num_mother_names)
     
-    # # Sobrenomes exclusivos da mãe
-    # mother_last_names = random.sample(set(unique_mother_surnames) - set(shared_surnames), num_mother_surnames - shared_num_mother_surnames)
+    # Sobrenomes compartilhados
+    # shared_last_names = random.sample(set(full_name.split()[1:]), shared_num_mother_surnames)
+    shared_last_names = random.sample(list(set(full_name.split()[1:])), shared_num_mother_surnames)
+    mother_last_names = random.sample(list(set(unique_mother_surnames) - set(shared_last_names)), num_mother_surnames - shared_num_mother_surnames)
+
+
     
-    # # Combinando o nome completo da mãe
-    # full_mother_name = ' '.join(first_mother_name + mother_last_names + shared_surnames)
+    # Sobrenomes exclusivos da mãe
+    # mother_last_names = random.sample(set(unique_mother_surnames) - set(shared_last_names), num_mother_surnames - shared_num_mother_surnames)
+    
+    # Combinando o nome completo da mãe
+    full_mother_name = ' '.join(first_mother_name + mother_last_names + shared_last_names)
     
     return full_mother_name
