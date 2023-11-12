9766e67fbb1cc578d4e83763819674f54933cf57
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-12 20:18:57 -0300

    Celular e telefones incluidos

--- Profile_Generator.py
+++ Profile_Generator.py
@@ -13,6 +13,7 @@ from educationLv_generator import generate_ramdom_educationLv
 from employment_generator import generate_ramdom_ocupation
 from mother_generator import generate_mother_by_name
 from telephone_number_generator import generate_brazilian_phone_number
+from telephone_generator import generate_brazilian_telephone_number
 
 # Solicita ao usuário o número de perfis a serem gerados
 num_perfil = int(input("Digite o número de perfis a serem gerados: "))
@@ -20,6 +21,19 @@ num_perfil = int(input("Digite o número de perfis a serem gerados: "))
 # Gera os perfis
 perfis_data = []
 for i in range(num_perfil):
+    # Códigos de DDD do Brasil
+    ddd_list = [
+        "11", "12", "13", "14", "15", "16", "17", "18", "19",
+        "21", "22", "24", "27", "28", "31", "32", "33", "34", "35",
+        "37", "38", "41", "42", "43", "44", "45", "46", "47", "48",
+        "49", "51", "53", "54", "55", "61", "62", "63", "64", "65",
+        "66", "67", "68", "69", "71", "73", "74", "75", "77", "79",
+        "81", "82", "83", "84", "85", "86", "87", "88", "89", "91",
+        "92", "93", "94", "95", "96", "97", "98", "99"
+    ]
+
+    # Gerar DDD aleatório
+    ddd = random.choice(ddd_list)
     gender = generate_random_gender()
     full_name = generate_name_by_gender(gender)
     cpf = generate_cpf()
@@ -29,11 +43,12 @@ for i in range(num_perfil):
     mae = generate_mother_by_name(full_name)
     educacao = generate_ramdom_educationLv(idade)
     ocupation = generate_ramdom_ocupation(idade)
+    telefone = generate_brazilian_telephone_number()
     celular = generate_brazilian_phone_number()
     valid_cep = generate_cep()
     perfis_data.append(
         {"id": i + 1, "Nome": full_name, "Idade": idade, "Gênero": gender, 
-         "Etnia": etnia, "Nome da Mãe": mae, "Educação": educacao, "Ocupação": ocupation, "Celular": celular, "Cpf": cpf, "CEP": valid_cep, "valid": is_valid})
+         "Etnia": etnia, "Nome da Mãe": mae, "Educação": educacao, "Ocupação": ocupation, "Telefone": ddd + telefone, "Celular": ddd + celular, "Cpf": cpf, "CEP": valid_cep, "valid": is_valid})
 
 # Cria um arquivo JSON com os perfis gerados
 json_filename = "perfis_generated.json"
@@ -47,12 +62,12 @@ wb = Workbook()
 ws = wb.active
 
 # Adicione cabeçalhos
-ws.append(["ID", "Nome", "Idade", "Gênero", "Etnia","Nome da Mãe", "Educação", "Ocupação","Celular", "CPF", "CEP"])
+ws.append(["ID", "Nome", "Idade", "Gênero", "Etnia","Nome da Mãe", "Educação", "Ocupação","Telefone","Celular", "CPF", "CEP"])
 
 # Adicione os perfis aos dados
 for perfil in perfis_data:
     ws.append([perfil["id"], perfil["Nome"], perfil["Idade"], perfil["Gênero"], 
-               perfil["Etnia"],perfil["Nome da Mãe"] , perfil["Educação"], perfil["Ocupação"],perfil["Celular"], perfil["Cpf"], perfil["CEP"]])
+               perfil["Etnia"],perfil["Nome da Mãe"] , perfil["Educação"], perfil["Ocupação"],perfil["Telefone"], perfil["Celular"], perfil["Cpf"], perfil["CEP"]])
 
 # Especifique o caminho do arquivo .xls
 xls_filename = "perfis_generated_openpyxl.xlsx"
--- telephone_generator.py
+++ telephone_generator.py
@@ -1,12 +1,12 @@
 import random
 
 # Função para gerar número de telefone brasileiro
-def generate_brazilian_phone_number():
+def generate_brazilian_telephone_number():
     
     # Gerar número aleatório com 8 dígitos
     number = ''.join(random.choices("0123456789", k=8))
 
     # Formatar o número de telefone
-    formatted_phone_number = f" 9{number[:4]}-{number[4:]}"
+    formatted_phone_number = f" {number[:4]}-{number[4:]}"
 
     return formatted_phone_number
