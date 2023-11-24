9c0362a684a9251bacf70f1d60a8691ab79ab3b1
Marcos Silva <marcosbenhurx@gmail.com>
2023-11-24 01:40:07 -0300

    Update employment_generator.py

--- employment_generator.py
+++ employment_generator.py
@@ -2,7 +2,7 @@ import random
 
 # Funçaõ para gerar ocupação aleatória com base nas porcentagens
 def generate_ramdom_ocuppation(idade):
-    if idade >= 14:
+    if idade >= 18:
         status = random.choices(["Desocupado", "Ocupado"], weights=[7.73, 92.27])[0]
         if status == "Ocupado":
             ocupation = random.choices(["Empregado: Setor privado (CLT)", "Empregado: Setor privado (sem CLT)", 
@@ -12,7 +12,7 @@ def generate_ramdom_ocuppation(idade):
                                     "Empregador: Sem CNPJ", "Autônomo: Com CNPJ", "Autônomo: Sem CNPJ",
                                     "Trabalhador familiar auxiliar"], weights=[37.36, 13.26, 1.44, 4.38, 1.39, 7.72, 3.1, 3.42, 0.79, 6.37, 19.11, 1.5])[0]
         else: ocupation = "Desocupado"
-    elif idade < 14:
+    elif idade < 18:
         ocupation = "Fora da força de trabalho"
     elif idade > 80:
         ocupation = "Aposentado"
