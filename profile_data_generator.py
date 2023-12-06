5557263f849fb6cfb43723d3ed47d62cb7ddc133
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-06 15:41:56 -0300

    Update nome de mãe

--- profile_data_generator.py
+++ profile_data_generator.py
@@ -4,17 +4,15 @@ from cpf_generator import generate_cpf
 from email_generator import *
 from password_generator import generate_password
 from cpf_generator import validate_cpf
-from age_generator import generate_random_age
-from age_generator import generate_birthday
+from age_generator import *
 from cep_generator import generate_cep
 from ethnicity_generator import generate_random_ethnicity
 from educationLv_generator import generate_ramdom_educationLv
 from employment_generator import generate_ramdom_ocuppation
-# from mother_generator import generate_mother_by_name
+from mother_generator import generate_mother_by_name
 from telephone_number_generator import generate_brazilian_phone_number
 from telephone_generator import generate_brazilian_telephone_number
 from ddd_generator import generate_ddd
-# from Data_Transfer_Object import ProfileDTO
 
 def generate_profiles(num_profiles):
     profiles = []
@@ -31,7 +29,7 @@ def generate_profiles(num_profiles):
         age = generate_random_age()
         birthday = generate_birthday(age)
         ethnicity = generate_random_ethnicity(gender)
-        # mother_name = generate_mother_by_name(full_name)
+        mother_name = generate_mother_by_name(full_name)
         education = generate_ramdom_educationLv(age)
         occupation = generate_ramdom_ocuppation(age)
         ddd = generate_ddd()
@@ -41,18 +39,9 @@ def generate_profiles(num_profiles):
 
         profiles.append({
             "id": i + 1, "Nome": full_name, "Idade": age, "E-mail": email, "Senha": password, "Data de Nascimento": birthday,
-            "Gênero": gender, "Etnia": ethnicity, #"Nome da Mãe": mother_name,
+            "Gênero": gender, "Etnia": ethnicity, "Nome da Mãe": mother_name,
             "Educação": education, "Ocupação": occupation, "Telefone": ddd + phone_number,
             "Celular": ddd + cell_number, "Cpf": cpf, "CEP": valid_cep, "valid": is_valid
         })
-
-        # # Crie uma instância do ProfileDTO
-        # profile_dto = ProfileDTO(
-        #     i + 1, full_name, age, birthday, gender, ethnicity,
-        #     education, occupation, ddd + phone_number, ddd + cell_number, cpf, valid_cep, is_valid
-        # )
-
-        # # Adicione o dicionário do DTO à lista de perfis
-        # profiles.append(profile_dto.to_dict())
 
     return profiles
