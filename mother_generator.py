d60576d7b6e1f0673fe0572cf8f6c96431a5512e
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-12-06 16:04:59 -0300

    Update2

--- mother_generator.py
+++ mother_generator.py
@@ -34,19 +34,15 @@ unique_mother_surnames = list(set(mother_surnames))
 
 
 def generate_mother_by_name(full_name):
-    num_names = random.choices([1, 2], weights=[75, 25])[0]
-    num_surnames = random.choices([1, 2, 3, 4], weights=[25, 60, 10, 5])[0]
     names = full_name.split(" ")
     
     if len(names) > 2:
-        first_name = random.choice(unique_mother_names, num_names)
+        first_name = random.choice(unique_mother_names)
         last_name = random.choice(names[2:])
-        unique_last_name = random.choice(unique_mother_surnames, num_surnames)
-        return first_name + " " + last_name + " " + unique_last_name
+        return first_name + " " + last_name
     elif len(names) == 2:
-        first_name = random.choice(unique_mother_names, num_names)
+        first_name = random.choice(unique_mother_names)
         last_name = random.choice(names[1:])
-        unique_last_name = random.choice(unique_mother_surnames, num_surnames)
-        return first_name + " " + last_name + " " + unique_last_name
+        return first_name + " " + last_name
     else:
-        return random.choice(unique_mother_names, num_names) + " " + random.choice(unique_mother_surnames)
+        return random.choice(unique_mother_names) + " " + random.choice(unique_mother_surnames)
